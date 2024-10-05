aae9f56371d0100809fd5cf4af97b7e26ea8955b
Devansh0012 <devanshdubey0012@gmail.com>
2024-10-06 03:55:30 +0530

    added affordable energy video

--- sdg_game_frontend/src/components/Module1.js
+++ sdg_game_frontend/src/components/Module1.js
@@ -4,11 +4,12 @@ import { useNavigate } from 'react-router-dom';
 import Modal from 'react-modal'; // Import Modal from react-modal
 import poverty from '../assets/videos/Social Equity and Well Being.mp4'
 import health from '../assets/videos/Governance and Ecosystems.mp4'
+import growth from '../assets/videos/Economic Growth and Infrastructure.mp4'
 
 const sdgGroups = [
   { title: "No Poverty & Zero Hunger", videoUrl: poverty },
   { title: "Good Health & Quality Education", videoUrl: health },
-  { title: "Affordable Energy & Economic Growth", videoUrl: "/path/to/video4.mp4" },
+  { title: "Affordable Energy & Economic Growth", videoUrl: growth },
   { title: "Climate Action & Life Below Water", videoUrl: "/path/to/video5.mp4" },
 ];
 
